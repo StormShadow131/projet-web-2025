988f93b47e1242a810b579c53284ffc4a603ba72
Shado <anais.q33@gmail.com>
2025-04-18 16:07:39 +0200

    Fix role bug

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -19,31 +19,36 @@ class DashboardController extends Controller
         // Static nombre
         $totalGroupes = 5;
 
-        $user = auth()->user();
-        $role = $user->school()->pivot->role;
-
-        // Redirect to dashboard admin view
-        if ($role == 'admin') {
-            return view('pages.dashboard.dashboard-admin', compact(
-                'totalCohorts',
-                'totalStudents',
-                'totalTeachers',
-                'totalGroupes'
-            ));
-        }
-        // Redirect to dashboard teacher view
-        elseif ($role == 'teacher') {
-            return view('pages.dashboard.dashboard-teacher');
-        }
-        // Redirect to dashboard student view
-        elseif ($role == 'student') {
-            return view('pages.dashboard.dashboard-student');
-        }
-        // No role
-        else {
-            abort(403);
-        }
-
+//        $user = auth()->user();
+//        $role = $user->school()->pivot->role;
+//
+//        // Redirect to dashboard admin view
+//        if ($role == 'admin') {
+//            return view('pages.dashboard.dashboard-admin', compact(
+//                'totalCohorts',
+//                'totalStudents',
+//                'totalTeachers',
+//                'totalGroupes'
+//            ));
+//        }
+//        // Redirect to dashboard teacher view
+//        elseif ($role == 'teacher') {
+//            return view('pages.dashboard.dashboard-teacher');
+//        }
+//        // Redirect to dashboard student view
+//        elseif ($role == 'student') {
+//            return view('pages.dashboard.dashboard-student');
+//        }
+//        // No role
+//        else {
+//            abort(403);
+//        }
+        return view('pages.dashboard.dashboard-admin', compact(
+            'totalCohorts',
+            'totalStudents',
+            'totalTeachers',
+            'totalGroupes'
+        ));
     }
 
     public function dashboard()
